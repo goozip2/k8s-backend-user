a1727e51bc7b08930cad7192d44f14932755e0d9
goozip2 <92193151+goozip2@users.noreply.github.com>
2025-06-11 16:48:10 +0900

    feat: build.gradle version 0.0.4
    - build.gradle 버전 업데이트
    - openfeign 의존성 추가
    - FeignClient 이용한 서비스 및 컨트롤러 메소드 작성

--- src/main/java/com/welab/k8s_backend_user/api/open/UserController.java
+++ src/main/java/com/welab/k8s_backend_user/api/open/UserController.java
@@ -5,18 +5,22 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import com.welab.k8s_backend_user.remote.alim.RemoteAlimService;
 
 @Slf4j
 @RestController
 @RequestMapping(value = "/api/user/v1", produces = MediaType.APPLICATION_JSON_VALUE)
 @RequiredArgsConstructor
 public class UserController {
+    private final RemoteAlimService remoteAlimService;
+
     @GetMapping(value = "/test")
     public String test() {
         return "Hello Kubernetes";
     }
     @GetMapping(value = "/hello")
     public ApiResponseDto<String> hello() {
-        return ApiResponseDto.createOk("웰컴 투 백엔드 유저");
+        String remoteMessage = remoteAlimService.hello().getData();
+        String userResponse = "웰컴 투 백엔드 유저. 리모트 알림 메시지= " + remoteMessage;return ApiResponseDto.createOk(userResponse);
     }
 }
